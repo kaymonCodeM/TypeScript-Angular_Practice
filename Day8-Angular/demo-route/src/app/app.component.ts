9b00df5597774d28fdfcf5f88dc8c07e1161616a
Kaymon McCain <kaymonCodeM@outlook.com>
2022-11-16 15:31:23 -0600

    Started doing the form for day 8

--- Day8-Angular/demo-route/src/app/app.component.ts
+++ Day8-Angular/demo-route/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
+
+interface UserDetails{
+  username: FormControl<string>,
+  password:FormControl<any>
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-route';
+  title = 'The Reactive Form';
+  loginForm = new FormGroup<UserDetails>({
+    username: new FormControl('',{nonNullable:true}),
+    password: new FormControl('',Validators.required)
+  })
+
+  get user(){
+    return this.loginForm.get('username');
+  }
+
+  loginUser(){
+    console.log(this.loginForm.value);
+  }
 }
--- Day8-Angular/form-demo/src/app/app.module.ts
+++ Day8-Angular/form-demo/src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    UserFormComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
